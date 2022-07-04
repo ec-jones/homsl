cc390a70c8d78d30cea7a35c83e2dc6e74fae4e3
= <=>
2022-07-03 17:54:37 -0700

    Tabling without continuations

--- src/HoMSL/Rewrite.hs
+++ src/HoMSL/Rewrite.hs
@@ -6,71 +6,83 @@ module HoMSL.Rewrite
   ( saturate) where
 
 import Control.Applicative
-import Control.Monad.Memoization
-import Control.Monad.RWS
+import Control.Monad.Logic
+import Control.Monad.Writer
 import Data.Foldable
 import Control.Monad.ST
-import qualified Data.HashMap.Lazy as HashMap
+import qualified Data.HashMap.Strict as HashMap
 import qualified Data.HashSet as HashSet
 import HoMSL.Syntax
 import Debug.Trace
 
+-- | Satuate a set of clauses.
 saturate :: [Formula] -> HashSet.HashSet AClause
-saturate clauses = runST $ mdo
-  table <- memo $ \p -> do
-    -- Select a clause defining the given predicate.
-    clause <- asum [ pure clause | clause <- clauses, 
-                                  let (xs, head@(App (Sym p') _), body) = viewClause clause,
-                                  p == p'
-                                  
-                    ]
+saturate = go HashMap.empty HashSet.empty
+  where
+    go :: HashMap.HashMap Formula (Maybe (Term Id)) -> HashSet.HashSet AClause -> [Formula] -> HashSet.HashSet AClause
+    go seen autos [] = autos
+    go seen autos (clause : clauses) =
+      case formulaToClause clause of
+        Nothing
+          | clause `HashMap.member` seen -> go seen autos clauses
+          | otherwise ->
+            let (clauses', selected) = stepWith autos clause
+             in go (HashMap.insert clause selected seen) autos (clauses' ++ clauses)
+        Just auto
+          | auto `HashSet.member` autos -> go seen autos clauses
+          | otherwise ->
+            -- Find, and resaturate, clauses that are relevant to the new automaton clause
+            let relevant = [ clause | (clause, Just selected) <- HashMap.toList seen,
+                                      relevantAtom selected auto ]
+                reducts = concatMap (fst . stepWith (HashSet.singleton auto)) relevant
+             in 
+              go seen (HashSet.insert auto autos) (reducts ++ clauses)
+    
+    stepWith :: HashSet.HashSet AClause -> Formula -> ([Formula], Maybe (Term Id))
+    stepWith autos clause =
+      let (xs, head, body) = viewClause clause
+          (reducts, First selected) = runWriter $ observeAllT (step autos xs body)
+       in (Clause xs (Atom head) <$> reducts, selected)
 
-    -- Rewrite the clause to automaton form.
-    let (xs, head, body) = viewClause clause
-    body' <- rewrite table xs body
-    let clause' = Clause xs (Atom head) body'
-    case formulaToClause clause' of
-      Nothing -> 
-        error ("Reduce is not automaton: " ++ show clause')
-      Just aclause ->
-        pure aclause
-
-  runMemo (table "q0")
-
--- | A table of clauses grouped by predicate.
-type Table s =
-  String -> Memo AClause s AClause
-
--- | Reduce a formula to automaton form.
-rewrite :: 
-  forall s.
-  Table s -> -- ^ Table of results
+-- | Make a single reduction step in a non-automaton body.
+-- The function also emits the selected atom (if it could be resolved with new top-level clauses).
+step :: 
+  HashSet.HashSet AClause -> -- ^ Global automaton clauses
   [Id] -> -- ^ Subjects of the formula.
   Formula -> -- ^ Body of a clause.
-  Memo AClause s Formula
-rewrite table subjects fm =
-  go (HashSet.fromList subjects) (\_ -> empty) fm
+  LogicT (Writer (First (Term Id))) Formula
+step autos subjects fm =
+  go (HashSet.fromList subjects) autos fm
   where
     go :: Scope -- ^ Variables in scope
-        -> Table s -- ^ Local clauses
+        -> HashSet.HashSet AClause -- ^ Local and global automaton clauses
         -> Formula -- ^ Formula to rewrite
-        -> Memo AClause s Formula 
-    go scope locals (Atom (App (Sym p) arg@(Apps (Sym f) ss))) = do
+        -> LogicT (Writer (First (Term Id))) Formula
+    go scope autos (Atom atom@(App (Sym p) arg@(Apps (Sym f) ss))) = do
       -- (Step)
-      AClause xs _ arg' body <- table p
+      lift (tell (pure atom))
+      
+      AClause xs p' arg' body <- msum (pure <$> HashSet.toList autos)
+      guard (p == p')
+
       inst <- match xs arg' arg
-      go scope locals (subst inst body)
-    go scope locals (Atom tm@(App (Sym p) arg@(Var x)))
-      | x `elem` subjects = pure (Atom tm)
+      pure (subst inst body)
+    go scope autos (Atom atom@(App (Sym p) arg@(Var x)))
+      | x `elem` subjects = empty
       | otherwise = do
         -- (Refl)
-        AClause xs _ arg' body <- locals p
+        AClause xs p' arg' body <- msum (pure <$> HashSet.toList autos)
+        guard (p == p')
+
         inst <- match xs arg' arg
-        go scope locals (subst inst body)
-    go scope locals (Atom (App (Sym p) arg@(Apps (Var x) ss)))
+        pure (subst inst body)
+    go scope autos (Atom atom@(App (Sym p) arg@(Apps (Var x) ss)))
       | x `elem` subjects = do
         -- (Assm)
-        clause@(AClause xs _ _ body) <- locals p <|> table p
+        lift (tell (pure atom))
+
+        AClause xs p' _ body <- msum (pure <$> HashSet.toList autos)
+        guard (p == p')
 
         -- Drop partially applied variables.
         let ys = drop (length xs - length ss) xs
@@ -85,46 +97,141 @@ rewrite table subjects fm =
             body' = subst rho (restrictBody ys body)
             head' = App (Sym p) (Apps (Var x) (map Var ys'))
 
-        body'' <- go scope locals (subst inst body')
-        pure (Conj [body'', Clause ys' (Atom head') body'])
+        pure (Conj [subst inst body', Clause ys' (Atom head') body'])
       | otherwise = do
         -- (Step)
-        AClause xs _ arg' body <- locals p
+        AClause xs p' arg' body <- msum (pure <$> HashSet.toList autos)
+        guard (p == p')
+
         inst <- match xs arg' arg
-        go scope locals (subst inst body) 
-    go scope locals (Atom _) = error "Atom is not well-formed!"
-    go scope locals (Conj []) = pure (Conj [])
-    go scope locals (Conj (fm : fms)) = do
-      fm' <- go scope locals fm
-      fms' <- go scope locals (Conj fms)
-      pure (Conj [fm', fms'])
-    go scope locals (Exists _ _) = error "Existentials are not yet supported!"
-    go scope locals (Clause xs head body)
+        pure (subst inst body)
+    go scope autos (Atom _) = error "Atom is not well-formed!"
+    go scope autos (Conj []) = empty
+    go scope autos (Conj (fm : fms)) =
+      case formulaToNestedClauses subjects fm of
+        Nothing -> do
+          -- (AndL)
+          fm' <- go scope autos fm
+          pure (Conj (fm' : fms))
+        Just _ -> do
+          -- (AndR)
+          fms' <- go scope autos (Conj fms)
+          pure (Conj [fm, fms'])
+    go scope autos (Exists _ _) = error "Existentials are not yet supported!"
+    go scope autos (Clause xs head body)
       | all (`notElem` xs) (freeVars head) = do
         -- (Scope1)
-        go scope locals head
+        pure head
       | Atom (App (Sym p) arg@(Apps (Var x) ss)) <- head,
-        x `elem` subjects,
-        all (`elem` map Var xs) ss = do
-          -- Head is already of the right form.
-          pure (Clause xs head body)
+        x `elem` subjects = empty
       | otherwise = do
           -- (ImpImp)
           let (scope', xs') = uniqAways scope xs
               rho = mkRenaming (zip xs xs')
               body' = subst rho body
+              
+          -- Extend antecedent with local automaton clauses
+          let Just autos' = formulaToNestedClauses xs' body'
 
-              -- Extend local clauses with clauses from the body.
-              locals' p = asum [ pure aclause | 
-                                      let Just clauses = formulaToNestedClauses xs' body',
-                                      aclause@(AClause _ p' (Apps (Var x') _) _) <- HashSet.toList clauses,
-                                      p == p'
-                                  ] <|> locals p
+          head' <- go scope' (autos' <> autos) (subst rho head)
+          pure (Clause xs' head' body')
 
-          head' <- go scope' locals' (subst rho head)
-          if all (`notElem` xs') (freeVars head')
-            then pure head'
-            else empty -- pure (Clause xs' head' body')
+-- | Check if the atom could be resolved with the clause.
+relevantAtom :: Term Id -> AClause -> Bool
+relevantAtom (App (Sym p) (Apps (Sym f) _)) (AClause _ p' (Apps (Sym f') _) _) =
+  p == p' && f == f'
+relevantAtom (App (Sym p) (Apps (Var x) ss)) (AClause xs p' _ _) =
+  p == p' && 
+    let ys = drop (length xs - length ss) xs
+     in sortArgs x.sort == map (.sort) ys
+relevantAtom _ _ = False
+
+-- -- | Reduce a formula to automaton form.
+-- rewrite :: 
+--   forall s.
+--   Table s -> -- ^ Table of results
+--   [Id] -> -- ^ Subjects of the formula.
+--   Formula -> -- ^ Body of a clause.
+--   Memo AClause s Formula
+-- rewrite table subjects fm =
+--   step (HashSet.fromList subjects) (\_ -> empty) fm
+--   where
+--     step :: Scope -- ^ Variables in scope
+--         -> Table s -- ^ Local clauses
+--         -> Formula -- ^ Formula to rewrite
+--         -> Memo AClause s Formula 
+--     step _ _ fm
+--       | Just _ <- formulaToNestedClauses subjects fm = pure fm
+--       | trace ("Step: " ++ show fm) False = undefined
+--     step scope locals (Atom (App (Sym p) arg@(Apps (Sym f) ss))) = do
+--       -- (Step)
+--       AClause xs _ arg' body <- table p
+--       inst <- match xs arg' arg
+--       step scope locals (subst inst body)
+--     step scope locals (Atom tm@(App (Sym p) arg@(Var x)))
+--       | x `elem` subjects = pure (Atom tm)
+--       | otherwise = do
+--         -- (Refl)
+--         AClause xs _ arg' body <- locals p
+--         inst <- match xs arg' arg
+--         step scope locals (subst inst body)
+--     step scope locals (Atom (App (Sym p) arg@(Apps (Var x) ss)))
+--       | x `elem` subjects = do
+--         -- (Assm)
+--         clause@(AClause xs _ _ body) <- locals p <|> table p
+
+--         -- Drop partially applied variables.
+--         let ys = drop (length xs - length ss) xs
+--         guard (sortArgs x.sort == map (.sort) ys)
+
+--         -- Create fresh instance.
+--         let (_, ys') = uniqAways scope ys
+--             rho = mkRenaming (zip ys ys')
+
+--         -- Build reduct  
+--         let inst = mkSubst (zip ys' ss)
+--             body' = subst rho (restrictBody ys body)
+--             head' = App (Sym p) (Apps (Var x) (map Var ys'))
+
+--         body'' <- step scope locals (subst inst body')
+--         pure (Conj [body'', Clause ys' (Atom head') body'])
+--       | otherwise = do
+--         -- (Step)
+--         AClause xs _ arg' body <- locals p
+--         inst <- match xs arg' arg
+--         step scope locals (subst inst body)
+--     step scope locals (Atom _) = error "Atom is not well-formed!"
+--     step scope locals (Conj []) = pure (Conj [])
+--     step scope locals (Conj (fm : fms)) = do
+--       fm' <- step scope locals fm
+--       fms' <- step scope locals (Conj fms)
+--       pure (Conj [fm', fms'])
+--     step scope locals (Exists _ _) = error "Existentials are not yet supported!"
+--     step scope locals (Clause xs head body)
+--       | all (`notElem` xs) (freeVars head) = do
+--         -- (Scope1)
+--         step scope locals head
+--       | Atom (App (Sym p) arg@(Apps (Var x) ss)) <- head,
+--         x `elem` subjects = do
+--           -- Head is already of the right form.
+--           pure (Clause xs head body)
+--       | otherwise = do
+--           -- (ImpImp)
+--           let (scope', xs') = uniqAways scope xs
+--               rho = mkRenaming (zip xs xs')
+--               body' = subst rho body
+
+--               -- Extend local clauses with clauses from the body.
+--               locals' p = asum [ pure aclause | 
+--                                       let Just clauses = formulaToNestedClauses xs' body',
+--                                       aclause@(AClause _ p' (Apps (Var x') _) _) <- HashSet.toList clauses,
+--                                       p == p'
+--                                   ] <|> locals p
+
+--           head' <- step scope' locals' (subst rho head)
+--           if all (`notElem` xs') (freeVars head')
+--             then pure head'
+--             else empty -- step scope locals (Clause xs' head' body')
 
 -- | @matchHead xs head tm@ finds instance of @head@ that matches @tm@ instantitates @xs@.
 -- N.B. The head is assumed to be shallow and linear.
